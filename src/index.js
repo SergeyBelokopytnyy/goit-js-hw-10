c03e05723863ff102d74693ea0a1b18a204a6b6e
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-02-19 00:50:06 +0500

    ищет страну по названию
    Пока только ищет одну страну. нужно доделать чтобы выводил список стран и ошибки.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import countryCard from '../src/country-card.hbs';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries.js';
+import getRefs from './get-refs.js';
 
 const DEBOUNCE_DELAY = 300;
+const refs = getRefs();
+
+// console.dir(refs.inputSearch);
+
+refs.inputSearch.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch() {
+  const name = refs.inputSearch.value.trim();
+  // console.log(name);
+  fetchCountries(name).then(renderCountryCard).catch(onFetchError);
+}
+
+function renderCountryCard(country) {
+  const markup = countryCard(country);
+  refs.countryInfo.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  console.log(error);
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}

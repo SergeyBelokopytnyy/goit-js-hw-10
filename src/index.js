772b6881ad571574ceaf371145ee517c974fef9d
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-02-20 23:15:51 +0500

    допиливает логику поиска.
    Если результат поиска больше 10 стран => сообщение
    Если одна страна => карточка добавляется в DOM
    В остальных случаях => списком пушим в массив и в DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './css/styles.css';
-import countryCard from '../src/country-card.hbs';
+import countryCard from '../src/templates/country-card.hbs';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries.js';
-import getRefs from './get-refs.js';
+import { fetchCountries } from './js/fetch-countries.js';
+import getRefs from './js/get-refs.js';
+import countriesList from '../src/templates/countries-card.hbs';
 
 const DEBOUNCE_DELAY = 300;
 const refs = getRefs();
@@ -19,11 +20,27 @@ function onSearch() {
 }
 
 function renderCountryCard(country) {
-  const markup = countryCard(country);
-  refs.countryInfo.innerHTML = markup;
+  const countryCount = Object.keys(country).length;
+  if (countryCount > 10) {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (countryCount === 1) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    console.log(countryCount);
+    const markup = countryCard(country);
+    refs.countryInfo.innerHTML = markup;
+  } else {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    const markup = [];
+    for (let count of country) {
+      markup.push(countriesList(count));
+    }
+    refs.countryList.innerHTML = markup.join('');
+  }
 }
 
 function onFetchError(error) {
-  console.log(error);
+  // console.log(error);
   Notiflix.Notify.failure('Oops, there is no country with that name');
 }
